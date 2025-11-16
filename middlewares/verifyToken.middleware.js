020185f6c6e67bdd48c8054c45e1df53bd85b1fa
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-16 15:49:32 +0530

    feat: add role based access control middleware

---
+++ middlewares/verifyToken.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+    try {
+        let token;
+        let authHeader = req.headers.authorization;
+
+        if (authHeader && authHeader.startsWith("Bearer")) {
+            token = authHeader.split(" ")[1];
+
+            if (!token) {
+                return res.status(403).json({ success: false, message: "Authorization denied: No token provided" });
+            }
+
+            const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decodedData) ;
+            req.user = decodedData;
+            next();
+        }
+        else {
+            return res.status(401).json({ success: false, message: "Authorization denied: Header missing or incorrect format" });
+        }
+    } catch (error) {
+        return res.status(401).json({ success: false, message: "Token is not valid or has expired" });
+    }
+}
+
+module.exports = verifyToken;
