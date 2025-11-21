45638fc2767d4d290e006db509585e3c3b999146
divyabagora-GKMIT <divyabagora@gkmit.co>
2025-11-21 10:18:22 +0530

    staging-workflow

--- config/config.js
+++ config/config.js
@@ -3,17 +3,23 @@ dotenv.config();
 
 
 module.exports = {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
-        define:{
-            underscored: true,
-            createdAt: "created_at",
-            updatedAt: "updated_at",
-            deletedAt: "deleted_at",
-            paranoid: true
-        }
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    define: {
+        underscored: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        deletedAt: "deleted_at",
+        paranoid: true
+    },
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 }
